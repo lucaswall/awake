4f730caff838506e425ddd5750691cf8e4b4d3dd
Lucas Wall <lucas.wall@lemonchiligames.com>
2016-01-29 23:12:54 -0300

    Sigil group test.

---
+++ src/Awake/Assets/Sources/SigilFragment.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SigilFragment : MonoBehaviour {
+
+	public Collider touchCollider;
+
+	void Update() {
+		for ( int i = 0; i < Input.touchCount; i++ ) {
+			Touch t = Input.GetTouch(i);
+			if ( IsValidTouch(t) ) ProcessTouch(t);
+		}
+	}
+
+	bool IsValidTouch(Touch touch) {
+		if ( touch.phase != TouchPhase.Began && touch.phase != TouchPhase.Moved ) 
+			return false;
+		Ray ray = Camera.main.ScreenPointToRay(touch.position);
+		RaycastHit hit;
+		return touchCollider.Raycast(ray, out hit, 100.0f);
+	}
+
+	void ProcessTouch(Touch touch) {
+		touchCollider.enabled = false;
+		Destroy(gameObject);
+	}
+
+}
