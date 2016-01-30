abedeffa37c18b570a57b3f2c0073064075b4b2c
Lucas Wall <lucas.wall@lemonchiligames.com>
2016-01-29 21:57:28 -0300

    Daemon art.

---
+++ src/Awake/Assets/Sources/Demon.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Demon : MonoBehaviour {
+
+	public SpriteRenderer leftEye;
+	public SpriteRenderer rightEye;
+
+	void Start() {
+		SetAlpha(0.0f);
+	}
+
+	void Update() {
+		SetAlpha(GetAlpha() + 0.1f * Time.deltaTime);
+	}
+
+	void SetAlpha(float a) {
+		Color c = leftEye.color;
+		c.a = a;
+		leftEye.color = c;
+		rightEye.color = c;
+	}
+
+	float GetAlpha() {
+		return leftEye.color.a;
+	}
+
+}
