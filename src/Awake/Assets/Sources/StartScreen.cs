71e08be79a0729288bdbe54ba9dcae6cc2cb0a51
Lucas Wall <lucas.wall@lemonchiligames.com>
2016-01-30 21:05:59 -0300

    Start screen canvas set to scale.

--- src/Awake/Assets/Sources/StartScreen.cs
+++ src/Awake/Assets/Sources/StartScreen.cs
@@ -1,12 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class StartScreen : MonoBehaviour {
 
+	public Text tapToStartText;
+	public float tapToStartSpeed;
+
+	float textCount;
+
+	void Start() {
+		textCount = tapToStartSpeed;
+	}
+
 	void Update() {
 		if ( Input.GetMouseButtonDown(0) ) {
 			SceneManager.LoadScene("Main");
+		}
+		textCount -= Time.deltaTime;
+		if ( textCount <= 0.0f ) {
+			textCount = tapToStartSpeed;
+			tapToStartText.enabled = ! tapToStartText.enabled;
 		}
 	}
 
