e161c61de6d5e711752ae1646dcfbbe6f7272a10
Lucas Wall <lucas.wall@lemonchiligames.com>
2016-01-31 11:29:22 -0300

    Title shake.

--- src/Awake/Assets/Sources/StartScreen.cs
+++ src/Awake/Assets/Sources/StartScreen.cs
@@ -8,20 +8,41 @@ public class StartScreen : MonoBehaviour {
 	public Text tapToStartText;
 	public float tapToStartSpeed;
 	public Animator animator;
-	public AudioSource audioSource;
+
+	public Transform gameTitle;
+	public float shakeAmplitude;
+	public float minShakeInterval;
+	public float maxShakeInterval;
+	public int shakeLoops;
+	public float shakeInterval;
 
 	float textCount;
 	bool started = false;
+	float timeToShake;
 
 	void Start() {
 		textCount = tapToStartSpeed;
+		timeToShake = Random.Range(minShakeInterval, maxShakeInterval);
 	}
 
 	void Update() {
+		CheckForTouch();
+		TapToStartBlink();
+		CheckTitleShake();
+	}
+
+	public void StartGame() {
+		SceneManager.LoadScene("Main");
+	}
+
+	void CheckForTouch() {
 		if ( Input.GetMouseButtonDown(0) && ! started ) {
 			started = true;
 			animator.SetTrigger("Intro");
 		}
+	}
+
+	void TapToStartBlink() {
 		textCount -= Time.deltaTime;
 		if ( textCount <= 0.0f ) {
 			textCount = tapToStartSpeed;
@@ -29,8 +50,20 @@ public class StartScreen : MonoBehaviour {
 		}
 	}
 
-	public void StartGame() {
-		SceneManager.LoadScene("Main");
+	void CheckTitleShake() {
+		timeToShake -= Time.deltaTime;
+		if ( timeToShake <= 0.0f ) {
+			timeToShake = Random.Range(minShakeInterval, maxShakeInterval);
+			StartCoroutine(MakeTitleShake());
+		}
+	}
+
+	IEnumerator MakeTitleShake() {
+		Vector3 pos = gameTitle.position;
+		for ( int i = 0; i < shakeLoops; i++ ) {
+			gameTitle.position = pos + (Vector3) Random.insideUnitCircle * shakeAmplitude;
+			yield return new WaitForSeconds(shakeInterval);
+		}
 	}
 
 }
